02550b1245494cafa73cd78317649efefe4ea9a0
XIA <koryxia@icloud.com>
2024-07-16 09:50:29 +0200

    Finished 28, 459

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i <= n - m; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
