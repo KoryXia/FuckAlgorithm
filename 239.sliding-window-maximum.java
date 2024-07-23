fbf1220043f2f6e21df4dc41970fba52791fab9d
XIA <koryxia@icloud.com>
2024-07-23 10:15:20 +0200

    Finished 239

---
+++ 239.sliding-window-maximum.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=239 lang=java
+ *
+ * [239] Sliding Window Maximum
+ */
+
+// @lc code=start
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+            }
+
+        });
+
+        for (int i = 0; i < k; i++) {
+            queue.add(new int[] { nums[i], i });
+        }
+        int[] res = new int[nums.length - k + 1];
+        res[0] = queue.peek()[0];
+        for (int i = k; i < nums.length; i++) {
+            queue.add(new int[] { nums[i], i });
+            while (queue.peek()[1] < i - k + 1) {
+                queue.poll();
+            }
+            res[i - k + 1] = queue.peek()[0];
+        }
+        return res;
+    }
+}
+// @lc code=end
