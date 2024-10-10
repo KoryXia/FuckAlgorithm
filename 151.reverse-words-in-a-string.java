04dce6ffb0b99cf99c8cebedc4bef2b30f47b05d
XIA <koryxia@icloud.com>
2024-10-10 13:44:41 +0800

    day8

---
+++ 151.reverse-words-in-a-string.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=151 lang=java
+ *
+ * [151] Reverse Words in a String
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = removeSpace(s);
+        reverseStr(sb, 0, sb.length() - 1);
+        reverseEachWord(sb);
+        return sb.toString();
+    }
+
+    private StringBuilder removeSpace(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (s.charAt(start) == ' ') start++;
+        while (s.charAt(end) == ' ') end--;
+        StringBuilder sb = new StringBuilder();
+        while (start <= end) {
+            char c = s.charAt(start);
+            if (c != ' ' || sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+            start++;
+        }
+        return sb;
+    }
+
+    public void reverseStr(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+
+
+    private void reverseEachWord(StringBuilder sb) {
+        int start = 0;
+        int end = 1;
+        int n = sb.length();
+        while (start < n) {
+            while (end < n && sb.charAt(end) != ' ') {
+                end++;
+            }
+            reverseStr(sb, start, end - 1);
+            start = end + 1;
+            end = start + 1;
+        }
+    }
+}
+// @lc code=end
+
