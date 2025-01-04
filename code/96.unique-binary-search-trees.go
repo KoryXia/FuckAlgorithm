fcfeec91ce8678fcdf01ddc0b56da225284bd1f5
XIA <koryxia@icloud.com>
2025-01-04 20:44:15 +0100

    day31

---
+++ code/96.unique-binary-search-trees.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=96 lang=golang
+ *
+ * [96] Unique Binary Search Trees
+ */
+
+// @lc code=start
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+// @lc code=end
+
--- code/96.unique-binary-search-trees.java
+++ code/96.unique-binary-search-trees.java
@@ -9,8 +9,7 @@ class Solution {
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
             }
@@ -19,9 +18,3 @@ class Solution {
     }
 }
 // @lc code=end
-
-0 = 0
-1 = 0 * 2
-2 = 1 * 1
-3 = 2 * 0
-dp[3] = dp[2] * dp[0] + dp[1] * dp[1] + dp[0] * dp[2];
