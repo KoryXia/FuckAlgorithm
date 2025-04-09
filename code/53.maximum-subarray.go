f1a08fe81469956b67d2ff803b30e7d12c75abc6
XIA <koryxia@icloud.com>
2025-04-09 11:11:27 +0200

    day40

--- code/53.maximum-subarray.go
+++ code/53.maximum-subarray.go
@@ -6,17 +6,19 @@
 
 // @lc code=start
 func maxSubArray(nums []int) int {
-	sum, res := 0, nums[0]
-	for i := 0; i < len(nums); i++ {
-		sum += nums[i]
-		if sum > res {
-			res = sum
-		}
-		if sum < 0 {
-			sum = 0
-		}
+	dp, res := make([]int, len(nums)), nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		res = max(res, dp[i])
 	}
 	return res
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
--- code/53.maximum-subarray.java
+++ code/53.maximum-subarray.java
@@ -7,17 +7,12 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int res = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            res = Math.max(res, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            res = Math.max(dp[i], res);
         }
         return res;
     }
